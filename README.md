1923995d42c9c364f3627a759ec000b98838570c
AdenikeSalau <kifademi@gmail.com>
2020-07-15 00:22:09 +0100

    bug: fixed responsiveness

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # drum-kit
+
+## How to use
+Press the corresponding keys on a keyboard to listen to different the sounds matched to them.
+A key is highlighted while it is playing. If key pressed does not match any of the ones provided, no response is given
+Not supported on mobile
+
+## Hosted Link
+https://drum-kit-js30.herokuapp.com/
--- js/script.js
+++ js/script.js
@@ -8,15 +8,25 @@ function removeTransition(e){
 function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+    //if key pressed not present on page, stop
     if (!audio) return;
+
+    //when playing key, add style
     key.classList.add("playing");
+
+    //reset playtime so key plays everytime it is called, regardless of if it is already playing or not
     audio.currentTime = 0;
     audio.play();
     
 }
 
 const keys = Array.from(document.querySelectorAll(".key"));
+
+//remove transition style after playing
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+//listen for key and play
 window.addEventListener('keydown', playSound);
 
 
